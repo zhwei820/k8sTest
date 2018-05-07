8cad48b7b948de3d922630e78eebf8096efd514c
Your Name <you@example.com>
2018-05-07 23:20:12 +0800

    12

--- kong/steps.sh
+++ kong/steps.sh
@@ -12,3 +12,34 @@ docker run --rm \
     -e "KONG_DATABASE=postgres" \
     -e "KONG_PG_HOST=kong-database" \
     192.168.1.5:5000/kong:v0.13 kong migrations up
+
+
+# run
+
+ docker run -d --name kong \
+    --link kong-database:kong-database \
+    -e "KONG_DATABASE=postgres" \
+    -e "KONG_PG_HOST=kong-database" \
+    -e "KONG_CASSANDRA_CONTACT_POINTS=kong-database" \
+    -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
+    -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
+    -e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
+    -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
+    -e "KONG_ADMIN_LISTEN=0.0.0.0:8001" \
+    -e "KONG_ADMIN_LISTEN_SSL=0.0.0.0:8444" \
+    -p 8000:8000 \
+    -p 8443:8443 \
+    -p 8001:8001 \
+    -p 8444:8444 \
+    192.168.1.5:5000/kong:v0.13
+
+
+# api
+
+ curl -i http://localhost:8001/
+
+# dashboard
+
+ docker run --rm -p 8080:8080 pgbi/kong-dashboard start \
+  --kong-url http://192.168.1.5:8001
+  --basic-auth user1=password1 user2=password2
