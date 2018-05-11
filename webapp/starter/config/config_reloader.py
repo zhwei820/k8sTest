916298a87553577baabaf07758b3164960027b6e
Your Name <you@example.com>
2018-05-11 21:37:48 +0800

    u

--- webapp/starter/config/config_reloader.py
+++ webapp/starter/config/config_reloader.py
@@ -1,7 +1,7 @@
 
 from multiprocessing import Process
 import os, time
-from starter.config.config_util import load_config_from_env
+from starter.config.config_util import load_config_from_file, setting_path
 
 import logging
 
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 
 
 # 子进程要执行的代码
-def run_loop(env_key="appsettings"):
+def run_loop():
     """
     死循环  定时检测配置是否更新
     :param env_key:
@@ -21,16 +21,18 @@ def run_loop(env_key="appsettings"):
         hot reload app
         :return:
         """
-        res = os.popen("kill -HUP `ps -ef|grep /uwsgi/| grep -v grep | awk '{print $2 }'`;").readlines()
+        killcmd = "kill -HUP `ps -ef|grep /uwsgi/| grep -v grep | awk '{print $2 }'`;"
+        res = os.popen(killcmd).readlines()
+
         _logger.info("应用配置更新!! \nnew_settings: %s , \nreload res: %s," % (new_settings, res))
 
     settings = ""
     while True:
-        new_settings = load_config_from_env(env_key)
+        new_settings = load_config_from_file(setting_path)
         if settings and new_settings and new_settings != settings:
             reload_app(new_settings)
         settings = new_settings
-        time.sleep(2)
+        time.sleep(5)
 
 
 def env_config_checker():
--- webapp/starter/config/config_util.py
+++ webapp/starter/config/config_util.py
@@ -8,33 +8,23 @@ _logger = logging.getLogger(__name__)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+setting_path = os.path.join(BASE_DIR, "settings.yaml")
 
 def load_config_from_file(file_path):
+    """
+        从（docker）文件中取配置, k8s pod中指定对应的configMap， 更新对应的configMap， 就能更新应用配置
+
+    :param file_path:
+    :return:
+    """
     setting_config = {}
     with codecs.open(file_path, mode='rb', encoding="utf-8") as f:
         setting_config.update(yaml.load(f))
     return setting_config
 
-def load_config_from_env(env_key = "appsettings"):
-    """
-    从（docker）环境变量中取配置, k8s pod中指定对应的configMap， 更新对应的configMap， 就能更新应用配置
-    :param env_key:
-    :return:
-    """
-    setting_config = {}
-    print(len(os.environ))
-    print(os.environ)
-
-    try:
-        env_val = os.environ.get(env_key, "")
-        setting_config = yaml.load(env_val)
-    except Exception as e:
-        _logger.error(e)
-    return setting_config
-
 
 def get_conf():
-    config = load_config_from_env() or load_config_from_file(os.path.join(BASE_DIR, "settings.yaml"))
+    config = load_config_from_file(setting_path)
 
     return config
 
