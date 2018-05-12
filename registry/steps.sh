3ef13f987d04028ae5ce578fe3faf607f198d02d
Your Name <you@example.com>
2018-05-12 21:29:41 +0800

    fix port

--- registry/steps.sh
+++ registry/steps.sh
@@ -1,17 +1,17 @@
 
 
-mkdir auth
+mkdir /etc/auth/ -p
 
-docker run --entrypoint htpasswd registry:2 -Bbn myuser mypassword > auth/htpasswd
+docker run --entrypoint htpasswd registry:2 -Bbn myuser mypassword > /etc/auth/htpasswd
 
-docker run -d -p 5000:5000 --restart=always --name registry_private  -v `pwd`/auth:/auth  -e "REGISTRY_AUTH=htpasswd"  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm"  -e "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd"  registry:2
+docker run -d -p 5000:5000 --restart=always --name registry_private  -v /etc/auth:/auth  -e "REGISTRY_AUTH=htpasswd"  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm"  -e "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd"  registry:2
 
 
 docker login localhost:5000
 
 
 docker run -d \
-  -p 8100:8100 \
+  -p 8981:80 \
   -e REGISTRY_HOST=192.168.1.5 \
   -e REGISTRY_PORT=5000 \
   -e REGISTRY_PROTOCOL=http \
