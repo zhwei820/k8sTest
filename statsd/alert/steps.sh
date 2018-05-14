324dc6c311aa991f0e558895d86401ba06dcdb3d
小周 <602068971@qq.com>
2018-05-14 12:11:30 +0800

    statsd alert

--- statsd/alert/steps.sh
+++ statsd/alert/steps.sh
@@ -1,1 +1,2 @@
-docker run -v /root/back/k8s/k8s-app/app/statsd/alert/config.json:/srv/alerting/etc/config.json deliverous/graphite-beacon
+docker run -v /root/back/k8s/k8s-app/app/statsd/alert/config.json:/srv/graphite_beacon/config.json \
+192.168.1.5:5000/beacon:v0.1
